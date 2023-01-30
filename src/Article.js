b9df2fd1e23cc2b96c8f03ae9d98ef311521721f
Ziru Zou <jiyucoding@yahoo.com>
2023-01-30 13:57:42 -0500

    complete lab

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+// make a component for article
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
+    <div>Dear reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+  )
 }
+
 
 export default Article;
